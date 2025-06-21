9dca8af5f4df0112117692ac2f9a33718d2a818d
woodland-wedding2026 <matthi.kim@pm.me>
2025-06-21 20:45:40 +0200

    Update app.rs

--- src/app.rs
+++ src/app.rs
@@ -80,7 +80,7 @@ impl eframe::App for TemplateApp {
 
         egui::CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
-            ui.heading("Woodland Wedding 2026 - Kim, Matthias und Yoshi == kimashi :)) ");
+            ui.heading("Woodland Wedding 2026 - Kim, Matthias und Yoshi == kimashi ");
 
             
             
