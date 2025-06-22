8943974dbf060d592bc1dda806867601d7385f8a
woodland-wedding2026 <matthi.kim@pm.me>
2025-06-22 20:16:26 +0200

    Update app.rs

--- src/app.rs
+++ src/app.rs
@@ -85,7 +85,7 @@ impl eframe::App for TemplateApp {
             let desired_size = egui::vec2(128.0, 128.0); 
             ui.add(egui::Image::new(collage).fit_to_exact_size(desired_size));
 
-            egui::Window::new("about the location").open(Some(true)).title_bar(true).show(ctx, |ui| {
+            egui::Window::new("about the location").open(true).title_bar(true).show(ctx, |ui| {
                 ui.label("tents and bungalows:");
                 
             });
