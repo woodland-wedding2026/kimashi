24aa8975fff3aaa6c07266157dc6894417e07af1
woodland-wedding2026 <matthi.kim@pm.me>
2025-06-24 19:59:20 +0200

    Update app.rs

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,3 @@
-
-
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 #[derive(serde::Deserialize, serde::Serialize)]
 #[serde(default)] // if we add new fields, give them default values when deserializing old state
