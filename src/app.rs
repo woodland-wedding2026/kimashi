c12dd0707b5fa89ae8b523540ffeb37bc3879b0c
woodland-wedding2026 <matthi.kim@pm.me>
2025-06-26 21:51:03 +0200

    Update app.rs

--- src/app.rs
+++ src/app.rs
@@ -71,28 +71,18 @@ impl eframe::App for TemplateApp {
 
         
         egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-
-        
-
-            
             // The top panel is often a good place for a menu bar:
-
             egui::menu::bar(ui, |ui| {
                 // NOTE: no File->Quit on web pages!
                 let is_web = cfg!(target_arch = "wasm32");
                 if !is_web {
                     ui.menu_button("File", |ui| {
-                        if ui.button("Quit").clicked() {
-                            ctx.send_viewport_cmd(egui::ViewportCommand::Close);
-                            
-                        }
+                        if ui.button("Quit").clicked() {ctx.send_viewport_cmd(egui::ViewportCommand::Close); }
                     });
                     ui.add_space(16.0);
                 }
-
                 egui::widgets::global_theme_preference_buttons(ui);
-            });
-        });
+            });});
 
 
         egui::SidePanel::left("bullet points").show(ctx, |ui| {
@@ -144,28 +134,28 @@ impl eframe::App for TemplateApp {
             
             
             
-            ui.horizontal(|ui| {
-                ui.label("This is how much I love you: ");
-                ui.text_edit_singleline(&mut self.label);
-                
-            });
+            //ui.horizontal(|ui| {
+            //    ui.label("This is how much I love you: ");
+            //    ui.text_edit_singleline(&mut self.label);
+            //    
+            //});
 
-            ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
-            if ui.button("Increment").clicked() {
-                self.value += 1.0;
-            }
+            //ui.add(egui::Slider::new(&mut self.value, 0.0..=10.0).text("value"));
+            //if ui.button("Increment").clicked() {
+            //    self.value += 1.0;
+            //}
 
-            ui.separator();
+            //ui.separator();
 
-            ui.add(egui::github_link_file!(
-                "https://github.com/emilk/eframe_template/blob/main/",
-                "Source code."
-            ));
+            //ui.add(egui::github_link_file!(
+            //    "https://github.com/emilk/eframe_template/blob/main/",
+            //    "Source code."
+            //));
 
-            ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
-                powered_by_egui_and_eframe(ui);
-                egui::warn_if_debug_build(ui);
-            });
+            //ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
+            //    powered_by_egui_and_eframe(ui);
+            //    egui::warn_if_debug_build(ui);
+            //});
         });
     }
 }
