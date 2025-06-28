91a2363611efea2029747fe04ed5e8d58045fedc
woodland-wedding2026 <matthi.kim@pm.me>
2025-06-28 10:41:40 +0200

    Update app.rs

--- src/app.rs
+++ src/app.rs
@@ -132,7 +132,7 @@ impl eframe::App for TemplateApp {
                 ehttp::fetch(request1, move |result: ehttp::Result<ehttp::Response>| {println!("Status code: {:?}", result.unwrap().status);});
                 self.value +=1;}
                 ui.label("you sent ");
-                ui.label(value.to_string());
+                ui.label(self.value.to_string());
                 ui.label("messages");
                 ui.hyperlink_to("see messages", "https://ntfy.sh/woodland");
             });
