dbc8f546a2dd310897edc4ba9e90c9b3c0c7ccd1
woodland-wedding2026 <matthi.kim@pm.me>
2025-10-28 20:40:20 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,8 @@ fn main() -> eframe::Result {
 
     let native_options = eframe::NativeOptions {
         viewport: egui::ViewportBuilder::default()
-            .with_inner_size([400.0, 300.0])
+            //.with_inner_size([400.0, 300.0])
+            .with_inner_size([1280.0, 1024.0])
             .with_min_inner_size([300.0, 220.0])
             .with_icon(
                 // NOTE: Adding an icon is optional
